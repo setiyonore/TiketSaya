a4c0acd46867895b2207facbdc6ae87b141837ec
setiyonore <setiyono.ressly@gmail.com>
2021-11-05 20:32:08 +0700

    XML Get Started

--- app/src/main/java/com/example/tiketsaya/SplashAct.java
+++ app/src/main/java/com/example/tiketsaya/SplashAct.java
@@ -2,7 +2,9 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashAct extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //redirect ke activity lain
+                Intent goGetStarted = new Intent(SplashAct.this,GetStartedAct.class);
+                startActivity(goGetStarted);
+                finish();
+            }
+        }, 3000);
     }
 }
