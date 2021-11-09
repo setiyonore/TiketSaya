66f2685bfb75b7794da44497ee76c8a68c71d710
setiyonore <setiyono.ressly@gmail.com>
2021-11-09 21:25:50 +0700

    XML Success Register

--- app/src/main/java/com/example/tiketsaya/RegisterTwoAct.java
+++ app/src/main/java/com/example/tiketsaya/RegisterTwoAct.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterTwoAct extends AppCompatActivity {
     LinearLayout btn_back_register_two;
+    Button btn_continue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,15 @@ public class RegisterTwoAct extends AppCompatActivity {
                 Intent gotoSignOne = new Intent(RegisterTwoAct.this,
                         RegisterOnceAct.class);
                 startActivity(gotoSignOne);
+            }
+        });
+        btn_continue = findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoExplore = new Intent(RegisterTwoAct.this,
+                        SuccessRegisterAct.class);
+                startActivity(gotoExplore);
             }
         });
     }
