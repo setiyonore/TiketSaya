3f48a5b22f8ae75b10197c09f0ea37952a180f48
setiyonore <setiyono.ressly@gmail.com>
2021-11-14 16:02:38 +0700

    XML Home Screen

--- app/src/main/java/com/example/tiketsaya/SigninAct.java
+++ app/src/main/java/com/example/tiketsaya/SigninAct.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SigninAct extends AppCompatActivity {
     TextView text_new_account;
+    Button btn_sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,15 @@ public class SigninAct extends AppCompatActivity {
                 Intent gotoRegisterOne = new Intent(SigninAct.this,
                         RegisterOnceAct.class);
                 startActivity(gotoRegisterOne);
+            }
+        });
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoHome = new Intent(SigninAct.this,HomeAct.class);
+                startActivity(gotoHome);
             }
         });
     }
