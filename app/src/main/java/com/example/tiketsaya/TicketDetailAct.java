cee8dbde610a67d6d31c64180557edada3dd067f
setiyonore <setiyono.ressly@gmail.com>
2021-11-22 22:37:32 +0700

    XML Checkout

--- app/src/main/java/com/example/tiketsaya/TicketDetailAct.java
+++ app/src/main/java/com/example/tiketsaya/TicketDetailAct.java
@@ -2,13 +2,25 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketDetailAct extends AppCompatActivity {
-
+    Button btn_buy_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+
+        btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+        btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoCheckout = new Intent(TicketDetailAct.this,TicketCheckoutAct.class);
+                startActivity(gotoCheckout);
+            }
+        });
     }
 }
