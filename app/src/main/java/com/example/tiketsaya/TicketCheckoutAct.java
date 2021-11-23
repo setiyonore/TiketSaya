2c7556f36206ee2a44ca19f0a42d27aaa10cf78c
setiyonore <setiyono.ressly@gmail.com>
2021-11-23 22:14:14 +0700

    XML Success Ticket

--- app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
+++ app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
@@ -2,13 +2,25 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketCheckoutAct extends AppCompatActivity {
-
+    Button btn_buy_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+        btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+        btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoSuccessTicket = new Intent(TicketCheckoutAct.this,
+                        SuccessBuyTicketAct.class);
+                startActivity(gotoSuccessTicket);
+            }
+        });
     }
 }
