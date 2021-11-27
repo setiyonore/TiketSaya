95da1f389ac6447a43ea5071e977d8488cc02c53
setiyonore <setiyono.ressly@gmail.com>
2021-11-27 20:42:51 +0700

    XML My Profile

--- app/src/main/java/com/example/tiketsaya/HomeAct.java
+++ app/src/main/java/com/example/tiketsaya/HomeAct.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import io.github.florent37.shapeofview.shapes.CircleView;
+
 public class HomeAct extends AppCompatActivity {
     LinearLayout btn_ticket_pisa;
+    CircleView btn_to_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,15 @@ public class HomeAct extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotoPisaTicket = new Intent(HomeAct.this,TicketDetailAct.class);
                 startActivity(gotoPisaTicket);
+            }
+        });
+
+        btn_to_profile = findViewById(R.id.btn_to_profile);
+        btn_to_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoProfile = new Intent(HomeAct.this, MyProfileAct.class);
+                startActivity(gotoProfile);
             }
         });
     }
